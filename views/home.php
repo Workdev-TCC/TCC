a4e4b0c3114e64bd16c5100790ec130f3c204b13
Gustavo Prado <gustavo.prado47@outlook.com>
2025-08-21 11:40:38 -0300

    design da home sobre
    sobre concluido sem mobile

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@
 </div>
 <section class="servicos">
     <div class="img">
-        <img src="<?php echo RAIZ_PROJETO;?>assets/img/hero.png" alt="ero">
+        <img src="<?php echo RAIZ_PROJETO;?>assets/img/servico.jpg" alt="erro">
     </div>
     <div class="servicos-text">
         <h2>Nossos Serviços </h2>
@@ -19,5 +19,16 @@
         </ul>
         <a class="btn btn-primary" href="#" role="button">Mais Informações</a>
         
+    </div>
+</section>
+<section class="sobre">
+    <div class="sobre-text">
+        <h1>Sobre Nos</h1>
+        <h2>Zupinturas</h2>
+        <p>Em 2009, Exupério Pereira, que até então era garçom, decidiu dar um passo ousado e fundou sua própria empresa no ramo de pintura. Inspirado pelos irmãos, que já atuavam na área, ele encontrou motivação e coragem para começar do zero.
+        Desde então, a ZUPINTURAS vem se destacando com responsabilidade, experiência e compromisso com a qualidade, transformando      imóveis com excelência e profissionalismo.</p>
+    </div>
+    <div class="sobre-img">
+        <img src="<?php echo RAIZ_PROJETO;?>assets/img/sobre.jpg" alt="erro">
     </div>
 </section>
