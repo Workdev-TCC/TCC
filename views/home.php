4c5d1757e4f965d072f024a4bc5fdb385a8b25c2
Gustavo Prado <gustavo.prado47@outlook.com>
2025-08-20 22:51:57 -0300

    Inicio no Layout home
    fiz o hero completo e iniciei a estrutura do "nossos serviços".

--- views/home.php
+++ views/home.php
@@ -1,5 +1,23 @@
+
 <div class="hero">
     <div class="hero-text">
-        <h1>Zupinturas</h1>        
+        <h1>Zupinturas</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, sapiente provident laudantium voluptatibus optio debitis nihil quasi nostrum, sit autem minima dolorem, voluptates possimus accusantium ipsam commodi doloremque libero perspiciatis.</p>
     </div>
 </div>
+<section class="servicos">
+    <div class="img">
+
+    </div>
+    <div class="servicos-text">
+        <h2>Nossos Serviços </h2>
+        <ul>
+            <li>Pintura Residencial</li>
+            <li>Pintura Comercial</li>
+            <li>Texturas e Efeitos Especias</li>
+            <li>Tratamento de Imperfeições</li>
+        </ul>
+        <a class="btn btn-primary" href="#" role="button">Mais Informações</a>
+        
+    </div>
+</section>
