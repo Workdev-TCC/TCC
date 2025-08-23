39967e8c65bf486eb06be082cfebaf60680cc1be
Gustavo Prado <gustavo.prado47@outlook.com>
2025-08-23 18:00:58 -0300

    FAQs finalizado
    Falta apenas mudar os textos. mobile  do faqs funcional

--- views/home.php
+++ views/home.php
@@ -32,3 +32,119 @@
         <img src="<?php echo RAIZ_PROJETO;?>assets/img/sobre.jpg" alt="erro">
     </div>
 </section>
+<section class="faqs">
+    <div class="faqs-text">
+        <h1>FAQs</h1>
+        <div class="accordions-box">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button
+                            class="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsetwo"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                        >
+                            Primeiro Item
+                        </button>
+                    </h2>
+                    <div
+                        id="collapsetwo"
+                        class="accordion-collapse collapse "
+                        aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample"
+                    >
+                        <div class="accordion-body">
+                            This is the first item's accordion body.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button
+                            class="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsethree"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="collapsethree"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample"
+                    >
+                        <div class="accordion-body">
+                            This is the first item's accordion body.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button
+                            class="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsefour"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="collapsefour"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample"
+                    >
+                        <div class="accordion-body" >
+                            This is the first item's accordion body.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button
+                            class="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsefive"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="collapsefive"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample"
+                    >
+                        <div class="accordion-body">
+                            This is the first item's accordion body.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+</section>
