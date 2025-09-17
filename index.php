1316e083496b2a78d4936e065bedc530f33100db
Stella Azevedo <costadeazevedostella@gmail.com>
2025-09-17 12:19:21 -0300

    update

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     include_once UTEIS;
 ?>
 <?php if (!empty($_SESSION['message'])) : ?>
-<div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
+<div class="alert message-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
     <?php echo $_SESSION['message']; ?>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-<?php clear_messages(); ?>
+<!-- <?php //clear_messages(); ?> -->
 <?php endif; ?>
 <?php
     include ("views/home.php");
