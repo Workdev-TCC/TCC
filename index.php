fd43dc57f51656b599e2fb9e994c66c43d1955be
Gustavo Prado <gustavo.prado47@outlook.com>
2025-09-18 12:09:27 -0300

    Modal Alert
    mexido em js tambem para troca de icone de acordo com a mensagem.

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 ?>
 <?php if (!empty($_SESSION['message'])) : ?>
 <div class="alert message-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
-    <?php echo $_SESSION['message']; ?>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="button-message"> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>
+    <div class="box-message"><strong><?php echo $_SESSION['message']; ?> <div class="icon-message" data-icon="<?php echo $_SESSION['type'];?>"><i id="icon-msg" class="fa-solid fa-circle-check"></i></div></strong></div>
 </div>
 <!-- <?php //clear_messages(); ?> -->
 <?php endif; ?>
