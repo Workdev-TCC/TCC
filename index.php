b2a09d794d65870f12f8f93ff4edfff4f610f1e3
Gustavo Prado <gustavo.prado47@outlook.com>
2025-09-19 17:31:21 -0300

    up

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     include_once UTEIS;
 ?>
 <?php if (!empty($_SESSION['message'])) : ?>
-<div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
-    <?php echo $_SESSION['message']; ?>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert message-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
+    <div class="button-message"> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>
+    <div class="box-message"><strong><?php echo $_SESSION['message']; ?> <div class="icon-message" data-icon="<?php echo $_SESSION['type'];?>"><i id="icon-msg" class="fa-solid fa-circle-check"></i></div></strong></div>
 </div>
-<?php clear_messages(); ?>
+<!-- <?php //clear_messages(); ?> -->
 <?php endif; ?>
 <?php
     include ("views/home.php");
