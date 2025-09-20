91c15866aa42edb83d62e5ab0a45b36c666b02f9
Gustavo Prado <gustavo.prado47@outlook.com>
2025-09-20 15:15:15 -0300

    Manipulação de banco de dados
    Funções save(),update e select funcional e padronizada para reutilizar em outras tabelas.

--- inc/uteis.php
+++ inc/uteis.php
@@ -1,21 +1,5 @@
 <!-- todas funções de utilidades, mascaras e etc devem estar aqui -->
 <?php 
- 
-    function verifica_login ($bd,$table,$campoemail,$camposenha,$login,$senha){
-        $sql= "SELECT * FROM $table WHERE $campoemail = :login AND $camposenha = :password LIMIT 1";
-        $stmt= $bd->prepare($sql);
-        $stmt->bindParam(":login",$login,PDO::PARAM_STR);
-        $stmt->bindParam(":password",$senha,PDO::PARAM_STR);
-        $stmt->execute();
-        if($stmt->rowCount() >0){
-            $dados=$stmt->fetch(PDO::FETCH_ASSOC);
-            return $dados;
-            
-        }
-        return null;
-    }
+    //necessario o objeto  Banco e conexão ja aberta
     
-
-
- 
  ?>
