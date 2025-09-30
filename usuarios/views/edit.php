d084e74f23c1bc7c7b3e372dfb2ac01744792fe4
Stella Azevedo <costadeazevedostella@gmail.com>
2025-09-30 10:53:01 -0300

    comecei a arrumar o designer edit

--- usuarios/views/edit.php
+++ usuarios/views/edit.php
@@ -12,21 +12,25 @@
 <?php clear_messages(); ?>
 <?php endif; ?>
 
-<form >
+<form action="<?php echo RAIZ_PROJETO;?>usuarios/edit.php" method="post" enctype="multipart/form-data">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Nome</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label for="nome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="nome" aria-describedby="emailHelp" name="nome">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="tel" class="form-label">Telefone</label>
+    <input type="text" name="telefone" class="form-control" id="tel" aria-describedby="emailHelp">
   </div>
+  <div class="mb-3">
+    <label for="foto" class="form-label">Foto</label>
+    <input type="file" name="foto" class="form-control" id="foto" aria-describedby="emailHelp">
+  </div>
+
   <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <input type="checkbox" class="form-check-input" id="confirma">
+    <label class="form-check-label" for="confirma">Confirma?</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 <?php
     include SIDEBAR;
