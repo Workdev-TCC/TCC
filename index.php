8d11574b04d2e9e396452722e2af3a2e3424c2ed
Caiooox1 <vitorcaio357@gmail.com>
2025-10-07 10:50:31 -0300

    tirei a linha lateral

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div class="button-message"> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>
     <div class="box-message"><strong><?php echo $_SESSION['message']; ?> <div class="icon-message" data-icon="<?php echo $_SESSION['type'];?>"><i id="icon-msg" class="fa-solid fa-circle-check"></i></div></strong></div>
 </div>
-<!-- <?php clear_messages(); ?> -->
+<?php clear_messages(); ?> 
 <?php endif; ?>
 <?php
     include ("views/home.php");
