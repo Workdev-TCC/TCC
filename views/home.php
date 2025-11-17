95d71ae8044aa6fabdc6409b8cca00a4224cb1e7
Stella Azevedo <costadeazevedostella@gmail.com>
2025-11-17 00:39:39 -0300

    loading

--- views/home.php
+++ views/home.php
@@ -1,9 +1,9 @@
-<!-- <div id="loading">
+<div id="loading">
   <div class="loader-content">
     <img src="<?php RAIZ_PROJETO;?>assets/img/logo.png" class="loading-img" alt="Logo">
     <div class="spinner"></div>
   </div>
-</div> -->
+</div>
 
 
 <div class="hero">
